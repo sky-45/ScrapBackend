64783a0e1694e97b4b5454f2d4a7d256c96773d8
Johan Agustin Barrera Alderete <ja_johan@hotmail.com>
2022-02-07 00:56:14 -0500

    cleaning code

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from ast import Str
-import profile
-from typing import Optional, List
+from typing import  List
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
@@ -9,23 +7,10 @@ from datetime import datetime
 from src.Profile import Profile
 import src.external.mongodb as dbHandler
 
-class Item(BaseModel):
-    id: int
-    generalInfo: dict
-    profilecard: dict
-    experiences: List[str] = []
-    education: List[str] = []
-
-class Item2(BaseModel):
-    data: List[dict] = []
-
 class Item3(BaseModel):
     data: List[dict] = []
 
-
-
 app = FastAPI()
-
 
 origins = [
     "*",
@@ -39,33 +24,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.get("/")
-def read_root():
-    now = datetime.now()
-    print(now.strftime("%d/%m/%Y %H:%M:%S"))
-    return {"Hello": "World"}
-
 @app.post("/data3/")
 def postData2(item: Item3):
-    #print("data raw: ", item.data)
     profile = Profile(item.data[0])
-    #print(profile.getDataProfiles())
     data = profile.getDataProfiles()
-    new_idx = dbHandler.getCurrentIndex() + 1
-    #print(new_idx,type(new_idx))
-    #data["id"] = Str(dbHandler.getCurrentIndex() + 1)
-
-    #send data to cosmosDB
     dbHandler.insertData(data)
-    print("data insertada")
-
-
     return {"status":"sucessfull"}
 
 @app.get("/data")
 def read_data():
     data = dbHandler.getProfiles()
-    
-
     return {"data":data}
